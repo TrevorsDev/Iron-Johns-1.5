54d643defbac867d45d4281527c853090c877d95
Trevor Johnson <trevor9j@protonmail.com>
2024-10-25 00:07:58 -0700

    added navigation modularity by separating html files, and loading the nav via js. Redesigned the folder/file strucutre

---
+++ scripts/loadNav.js
@@ -0,0 +1,14 @@
+// Listens for the DOMContentLoaded event, which fires when the HTML is fully loaded and parsed but before images, stylesheets, or subframes have loaded. Waiting for this event ensures that the JavaScript only runs once the DOM is ready, preventing errors from trying to access elements that haven’t yet loaded.
+document.addEventListener("DOMContentLoaded", function() {
+  // Fetch nav.html file and load it to index.html where the div with an id of "nav-placeholder" exists
+  fetch('components/nav.html')
+   .then(response => response.text())
+   .then(data => {
+      // Replace placeholder with fetched data
+      document.getElementById("nav-placeholder").innerHTML = data;
+    })
+   .catch(error => console.error('Error loading navigation bar', error));
+});
+
+
+  
