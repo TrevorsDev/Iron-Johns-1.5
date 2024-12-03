adfa8398adcdfe2cc373b08dabd66384b9f625d8
Trevor Johnson <trevor9j@protonmail.com>
2024-12-03 16:48:33 -0700

    added positioning and styling to the maps and oporations-card container.

--- public/scripts/loadApi.js
+++ public/scripts/loadApi.js
@@ -2,18 +2,18 @@
 /* JavaScript file to request the API key from the server dynamically: */
 /* For a restaurant website like yours, client-side integration with restricted API keys is industry-standard and safe! */
 
+/*Fetching the api key from the server. Lines 6-10*/
 async function loadGoogleMaps() {
   try {
     const response = await fetch('/api/key');
     const data = await response.json();
     const apiKey = data.apiKey; // Get the key from the server response
 
-
     const iframe = document.createElement('iframe');
     iframe.src = `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=5350 E Broadway #128, Tucson, AZ 85711:ChIJN1t_tDeuEmsRU9sPDbDGBsD8`;
 
-    iframe.width = "900";
-    iframe.height = "750";
+    iframe.width = "800";
+    iframe.height = "650";
     iframe.style.border = "0";
     iframe.loading = "lazy";
     iframe.allowFullscreen = true;
@@ -24,5 +24,6 @@ async function loadGoogleMaps() {
     console.error('Error loading Google Maps:', error);
   }
 }
+
 
 loadGoogleMaps();
