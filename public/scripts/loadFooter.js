84eaccce0d0d326cffa575ec7e03791caf8d4a9d
Trevor Johnson <trevor9j@protonmail.com>
2024-12-18 22:18:55 -0700

    remove uneeded footer.css styling, change navigation to a fixed styling using px for height instead of vh, correct file paths inside nav.html  order.html, loadFooter.js, loadNav.js, add order.html content, add order.css styling

--- public/scripts/loadFooter.js
+++ public/scripts/loadFooter.js
@@ -1,7 +1,7 @@
 // Listens for the DOMContentLoaded event, which fires when the HTML is fully loaded and parsed but before images, stylesheets, or subframes have loaded. Waiting for this event ensures that the JavaScript only runs once the DOM is ready, preventing errors from trying to access elements that haven’t yet loaded.
 document.addEventListener("DOMContentLoaded", function () {
   // Fetch nav.html file and load it to index.html where the div with an id of "nav-placeholder" exists
-  fetch('components/footer.html')
+  fetch('../components/footer.html')
     .then(response => response.text())
     .then(data => {
       // Replace placeholder with fetched data
